83129178f173f90058c077ce7ecd5145335a3359
Adriel-Nicolau <44787848+Adriel-Nicolau@users.noreply.github.com>
2018-11-21 22:14:49 +0000

    primeiro

---
+++ Residual/Residual/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Residual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Residual.Controllers
+{
+    public class HomeController : Controller
+    {
+        ModelContext db = new ModelContext();
+        public ActionResult Index()
+        {
+            Teste t = new Teste();
+            t.id = 1;
+
+            db.testes.Add(t);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Residual/Residual/Models/teste.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace Residual.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class Teste
+    {
+       [Key]
+        public int id { get; set; } 
+    }
+
+}
