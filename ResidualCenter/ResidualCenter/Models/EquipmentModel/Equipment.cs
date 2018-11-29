5b90eab7e1234bf340244246e894f14144fda848
Adriel-Nicolau <44787848+Adriel-Nicolau@users.noreply.github.com>
2018-11-29 01:14:49 +0000

    versao inicial de trabalho
    BD criada com contexto

---
+++ ResidualCenter/ResidualCenter/Models/EquipmentModel/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ResidualCenter.Models
+{
+    public class Equipment
+    {
+        public int ID { get; set; }
+        [Required]
+        public int EquipmentTypeID { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public bool Using { get; set; }
+
+        public virtual ICollection<Service> Services { get; set; }
+        public virtual EquipmentType EquipmentType { get; set; }
+    }
+}
